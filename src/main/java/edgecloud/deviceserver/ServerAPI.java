d006f4197ba1e31dc460dec3ad998034b5c0db20
jaluo <jaluo514@163.com>
2017-08-30 18:41:29 +0800

    add device server
     device server is a message system, frontend can use it's interface
      like this:
          ServerAPI  serverAPI = new ServerAPI();
           serverAPI.getAllDevice()
    
    two interface:
    1. sendMessage
    2. getAllDevic
    
    on the other hand, device server will manage the connection of device
    include device register, ping

---
+++ src/main/java/edgecloud/deviceserver/ServerAPI.java
@@ -0,0 +1,69 @@
+package edgecloud.deviceserver;
+
+import edgecloud.deviceserver.server.Device;
+import edgecloud.deviceserver.server.DeviceContext;
+import edgecloud.deviceserver.server.Message;
+import org.apache.log4j.Logger;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class ServerAPI {
+    private static Logger logger = Logger.getLogger(ServerAPI.class);
+
+    /**
+     *  sync event need wait the message result.
+     * @param type send funciton: 0, send sync event: 1, send async event: 2
+     * @param deviceId
+     * @param content
+     * @return
+     */
+    public String sendMessage(int type, String deviceId, String content) throws InterruptedException {
+        String msgId = UUID.randomUUID().toString();
+        if (type == 0 || type == 1) {
+            Message.ServiceMessage serverMessage = Message.ServiceMessage.newBuilder()
+                    .setMessageId(msgId)
+                    .setDeviceId(deviceId)
+                    .setType(type)
+                    .setContent(content)
+                    .build();
+            DeviceContext.sendMessage(serverMessage);
+            return msgId;
+        } else if (type == 2) {
+            Message.ServiceMessage serviceMessage = Message.ServiceMessage.newBuilder()
+                    .setMessageId(msgId)
+                    .setDeviceId(deviceId)
+                    .setType(type)
+                    .setContent(content)
+                    .build();
+            DeviceContext.sendMessage(serviceMessage);
+
+            // FIXME: ???
+            int tryCount = 10;
+            String result = "get message result failed";
+            while (tryCount > 0) {
+                Message.ClientMessage message = DeviceContext.eventResultMap.get(msgId);
+                if (message != null) {
+                    result = message.getContent();
+                    return result;
+                }
+
+                Thread.sleep(500);
+            }
+            return result;
+        } else {
+            return "event type is invalid";
+        }
+    }
+
+    // TODO: filter
+    public List<Device> getAllDevice(){
+        List<Device> devices = new ArrayList<Device>();
+        for(Device device: DeviceContext.onlineDevice.values()){
+            devices.add(device);
+        }
+        return devices;
+    }
+
+}
--- src/main/java/edgecloud/lambda/LambdaApplication.java
+++ src/main/java/edgecloud/lambda/LambdaApplication.java
@@ -1,19 +1,31 @@
 package edgecloud.lambda;
 
+import edgecloud.deviceserver.server.Device;
+import edgecloud.deviceserver.server.DeviceServer;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 
-@SpringBootApplication()
+@SpringBootApplication
+@ComponentScan
 public class LambdaApplication {
 
-    public LambdaApplication() {
 
-    }
 
     public static void main(String args[]) {
         init();
-        SpringApplication.run(LambdaApplication.class, args);
+
+//        SpringApplication.run(LambdaApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(LambdaApplication.class, args);
+        Thread deviceServer = new Thread(ctx.getBean(DeviceServerRun.class));
+        deviceServer.start();
     }
 
     // TODO
